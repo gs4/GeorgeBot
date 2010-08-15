194500d958718c880cc28a73367d9c767b400743
Gordon Sommers <gordon.sommers@gmail.com>
2010-08-15 00:30:57 -0400

    splitting

--- george.hs
+++ george.hs
@@ -1,55 +1,9 @@
-import Data.List
-import Network
-import System.IO
-import qualified Data.Map as Map
-import System.Environment (getArgs, getProgName)
-import Text.Printf
-import Control.Monad
-import Control.Monad.State
-import Control.Monad.Reader
-import Control.Monad.Cont
+import IRCBot
+
 import System.Exit
-import System.Random
-
-----------------------
-
-data Bot = Bot { _socket :: Handle,
-                 _port :: Int,
-                 _server :: String,
-                 _chan :: String,
-                 _nick :: String,
-                 _uname :: String
-                 } 
-
-type BotKey = String
-type BotVal = String
-type RandList = [Float]
-type Hook = Message -> BotAwesome Message
-
-data BotData = BotData { 
-  _rands :: RandList,
-  _map   :: (Map.Map BotKey BotVal),
-  _hooks :: Hook
-  }
-               
-type BotAwesome = StateT BotData (ReaderT Bot IO)
-
----------------------
-
-             {-type    nick   name   host   to     msg   -}
-data Message = PrivMsg String String String String String
-             | ServMsg String 
-             deriving Show
-
----------------------
-
-runBot :: BotAwesome () -> Bot -> Hook -> IO ()
-runBot codes defaultBot hook = do init <- initialBot hook
-                                  runReaderT (fst `fmap` (runStateT codes init)) defaultBot
-
-initialBot :: Hook -> IO BotData
-initialBot hook = do gen <- getStdGen
-                     return $ BotData (randoms gen) Map.empty hook
+import System.Environment
+import Data.List
+import Control.Monad
 
 main :: IO ()
 main = do
@@ -57,137 +11,18 @@ main = do
   pname <- getProgName
   when (length args /= 1) (putStrLn ("Usage: " ++ pname ++ " <channel>") >> exitWith (ExitFailure (0-1)))
   let chan = args !! 0
-  runProg chan "irc.factset.com" 6667 "george" "gsommers"
+  runProg chan "irc.freenode.com" 6667 "karavaev" "gs4"
 
 runProg :: String -> String -> Int -> String -> String -> IO ()
 runProg chan' server port nick uname = do
   let chan = if chan' !! 0 == '#' then chan' else '#':chan'
   putStrLn $ "Connecting to channel " ++ chan ++ " on server " ++ server ++ ":" ++ show port
   h <- ircConnect server chan port nick uname
-  return ()
-  runBot (listener) (Bot h port server chan nick uname) georgeHooks
-
-ircConnect :: String -> String -> Int -> String -> String -> IO Handle
-ircConnect server chan port nick uname = do
-  h <- connectTo server ((PortNumber . fromIntegral) port)
-  hSetBuffering h NoBuffering
-  echoWrite h "NICK" nick
-  echoWrite h "USER" (nick ++ " 0 * :" ++ uname)
-  echoWrite h "JOIN" chan
-  return h
-  
-ircWrite :: Bool -> Handle -> String -> String -> IO ()
-ircWrite echo handle s t = do
-  hPrintf handle "%s %s\r\n" s t
-  printf       "> %s %s\n"   s t
-
-echoWrite = ircWrite True
-
-botWrite :: String -> String -> BotAwesome ()
-botWrite s t = do
-  sock <- _socket `fmap` ask
-  liftIO $ echoWrite sock s t
-  
-modifyRands f (BotData x y z) = BotData (f x) y z
-modifyMap f (BotData x y z) = BotData x (f y) z
-modifyHooks f (BotData x y z) = BotData x y (f z)
-  
-botPut :: BotKey -> BotVal -> BotAwesome ()
-botPut key val = modify $ (modifyMap $ Map.insert key val)
-                    
-botGet :: BotKey -> BotAwesome (Maybe BotKey)
-botGet key = (Map.lookup key) `fmap` (gets _map)
-                 
-botRand :: BotAwesome Float
-botRand = do x <- head `fmap` gets _rands
-             modify $ modifyRands $ tail
-             return x
-
-listener :: BotAwesome ()
-listener = do sock <- _socket `fmap` ask
-              forever $ do
-                line <- liftIO $ hGetLine sock
-                let s = init line
-                messageHandler $ splitInput s
-                liftIO $ putStrLn s
-        where
-          forever a = do a; forever a
-
-privMsg :: String -> BotAwesome ()
-privMsg s = do 
-  chan <- _chan `fmap` ask
-  botWrite "PRIVMSG" (chan ++ " :" ++ s)
-
-messageHandler :: Message -> BotAwesome ()
-messageHandler (ServMsg input) | "PING :" `isPrefixOf` input = botWrite "PONG"  (':' : drop 6 input)
-messageHandler (ServMsg _    ) = return ()
-messageHandler msg = eval msg
-
-splitInput :: String -> Message
-splitInput line = (`runCont` id) $ do
-  callCC $ \exit -> do
-    let (nick, rest) = span (\c -> c /= '!') (tail line)
-    when (null rest) (exit $ ServMsg line)
-    let (name', rest') = span (\c -> c /= '@') (tail rest)
-    let name = tail name'
-    when (null rest') (exit $ ServMsg line)      
-    let (host', cmd') = span (\c -> c /= ' ') rest'
-    let host = tail host'
-    let cmd = tail cmd'
-    return $ makeMessage nick name host cmd
-      where
-        makeMessage :: String -> String -> String -> String -> Message
-        makeMessage nick name host cmd =
-          let cmdWords = words cmd
-          in case (head cmdWords) of
-            "PRIVMSG" -> parsePrivMsg nick name host (unwords $ tail cmdWords)
-            _ -> ServMsg line
-        parsePrivMsg :: String -> String -> String -> String -> Message
-        parsePrivMsg nick name host str =
-          let (target:msg') = words str
-              msg = tail (unwords msg')
-          in PrivMsg nick name host target msg
-
-                                  
-eval :: Message -> BotAwesome ()
-eval m@(PrivMsg fnick fname fhost fto fmsg)
-  | fmsg == "!quit" = botWrite "QUIT" ":Exiting"
-  | "!id" `isPrefixOf` fmsg = privMsg $ (unwords . tail. words) fmsg
-  | "!put" `isPrefixOf` fmsg = do let rest = (unwords . tail . words) $ fmsg
-                                  putToMap rest
-                                  chan <- _chan `fmap` ask
-                                  privMsg "saved!"
-  | "!get" `isPrefixOf` fmsg = do let rest = (unwords . tail . words) $ fmsg
-                                  val <- getFromMap rest
-                                  let result = case val of
-                                        Nothing -> "null"
-                                        Just str -> str
-                                  chan <- _chan `fmap` ask
-                                  privMsg result
-  | "!names" `isPrefixOf` fmsg = do botWrite "NAMES" ""
-  | "!rand" `isPrefixOf` fmsg = do let n = read $ (head . tail . words) $ fmsg
-                                   x <- botRand
-                                   privMsg $ "roll: " ++ (show $ truncate $ n * x)
-  | otherwise = gets _hooks >>= (\f -> f m) >> return ()
-eval x = return ()
-
-
-putToMap :: String -> BotAwesome ()
-putToMap rest = botPut key val
-  where key = takeWhile (/=' ') rest 
-        val = dropWhile (/=' ') rest
-
-getFromMap :: String -> BotAwesome (Maybe BotVal)
-getFromMap rest = botGet rest
-
-addHook :: (Message -> BotAwesome Message) -> BotAwesome ()
-addHook hook = do modify $ modifyHooks $ (composeHooks hook)
-
-composeHooks h1 h2 x = h1 x >>= h2
-
+  runBot (Bot h port server chan nick uname) georgeHooks
 
 georgeHooks :: Hook
 georgeHooks msg = return msg >>= v2w >>= randomQuestions >>= doEmotes >>= askWhy
+                  >>= doEmotes >>= disagree >>= disagree2 >>= disagree3
                     
 v2w m@(PrivMsg nick name host to msg) = 
   do if "vodka" `elem` (words . stripPunctuation) msg
@@ -210,18 +45,39 @@ randomQuestions m@(PrivMsg nick name host to msg) =
 doEmotes m@(PrivMsg nick name host to msg) = 
   do x <- botRand
      if x <= 0.01
-       then privMsg "guys"
+       then privMsg "guys..."
        else return ()
      return m
      
 askWhy m@(PrivMsg nick name host to msg) =     
   do myName <- asks _nick
      if (myName `isInfixOf` msg)
-       then privMsg "Why?"
+       then privMsg "what?"
        else return ()
      return m
-     
 
+disagree m@(PrivMsg nick name host to msg) = 
+  do x <- botRand
+     if x <= 0.01
+       then privMsg "that's not right"
+       else return ()
+     return m
+
+disagree2 m@(PrivMsg nick name host to msg) = 
+  do x <- botRand
+     if x <= 0.01
+       then privMsg "no?"
+       else return ()
+     return m
+
+disagree3 m@(PrivMsg nick name host to msg) = 
+  do x <- botRand
+     if x <= 0.01
+       then privMsg "incorrect"
+       else return ()
+     return m
+
+     
 stripPunctuation [] = []
 stripPunctuation (a:str) = if a `elem` "!.,:;?-()-_=+~'\"[]{}\\|<>/" 
                             then stripPunctuation str 
